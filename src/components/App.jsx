62e551644152d2eeebf56de109f68fc92d0dbb3a
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-09-16 16:02:55 +0300

    add all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+import { BreedSelect } from './BreedSelect';
+import { Layout } from './Layout';
+import { fetchDogByBreed } from '../api';
+import { Dog } from './Dog';
+import { DogSkeleton } from './DogSkeleton';
+import { ErrorMessage } from './ErrorMessage';
+
 export const App = () => {
+  const [dog, setDog] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDog = async breedId => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const dog = await fetchDogByBreed(breedId);
+      setDog(dog);
+    } catch (error) {
+      setError(
+        'У нас не получилось взять данные о собачке, попробуйте еще разочек 😇'
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <BreedSelect onSelect={fetchDog} />
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      {isLoading && <DogSkeleton />}
+      {dog && !isLoading && <Dog dog={dog} />}
+    </Layout>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+// import { App } from 'components/HTTPReqAbort';
+// import { App } from 'components/HTTPReqAbortOnEvent';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
